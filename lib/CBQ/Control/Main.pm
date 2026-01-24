3521f26f634706f885de061e09cf9c04626e1eeb
Gryphon Shafer <gryphon@goldenguru.com>
2026-01-24 15:59:17 -0800

    Force tempfile to string

--- lib/CBQ/Control/Main.pm
+++ lib/CBQ/Control/Main.pm
@@ -191,7 +191,7 @@ sub download ($self) {
         my $dbh  = $self->stash('user')->dq->clone({ sqlite_open_flags => SQLITE_OPEN_READONLY });
         my $temp = tempfile( SUFFIX => '.sqlite' );
 
-        $dbh->sqlite_backup_to_file($temp);
+        $dbh->sqlite_backup_to_file( $temp->to_string );
         $dbh->disconnect;
 
         $self->res->headers->content_type('application/x-sqlite');
